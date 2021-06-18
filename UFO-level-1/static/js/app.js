87beb7051d2c7f2c63f38992bedc3e2bdd395748
rromang <80008461+rromang@users.noreply.github.com>
2021-06-17 22:24:58 -0700

    finish ufo filter

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,66 @@
-// from data.js
-var tableData = data;
+// Get a reference to the table body
+var tbody = d3.select("tbody");
 
-// YOUR CODE HERE!
+var sight = data;
+
+// Console.log the weather data from data.js
+console.log(sight);
+
+sight.forEach(function(ufoReport) {
+  console.log(ufoReport);
+  var row = tbody.append("tr");
+  Object.entries(ufoReport).forEach(function([key, value]) {
+    console.log(key, value);
+    // Append a cell to the row for each value
+    // in the weather report object
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+// var form = d3.select("#form-control");
+
+// Create event handlers 
+button.on("click", runEnter);
+// form.on("submit",runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select(".form-control");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(sight);
+
+  var filteredData = sight.filter(entry => entry.datetime === inputValue);
+//   var filteredData = people.filter(person => person.bloodType === inputValue);
+
+  console.log(filteredData);
+  
+  var tbody = d3.select("tbody")
+  tbody.html("");
+
+  filteredData.forEach(function(ufoReport) {
+    console.log(ufoReport);
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      // in the weather report object
+      var cell = row.append("td");
+      cell.text(value);
+    });
+});
+};
+
